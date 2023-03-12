c9a94717f93cbce7bf4245c4396d7f064bad91fd
Jaroslavna Kuptsova <55083918+ykuptsova@users.noreply.github.com>
2023-03-12 22:51:32 +0200

    movie-cards implemented

---
+++ src/components/MovieCardsList/MovieCardsList.js
@@ -0,0 +1,12 @@
+import "./MovieCardsList.css";
+import MovieCards from '../MovieCards/MovieCards'
+
+function MovieCardsList(props) {
+  return (
+    <section className="movie-cards-list">
+      <MovieCards/>
+    </section>
+  )
+}
+
+export default MovieCardsList;
