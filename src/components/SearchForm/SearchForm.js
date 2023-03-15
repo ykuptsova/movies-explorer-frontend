6e541ba1274e3115d72e0cd2224fe86837fde85d
Jaroslavna Kuptsova <55083918+ykuptsova@users.noreply.github.com>
2023-03-15 13:00:04 +0200

    search-form implemented

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useState } from 'react';
+import './SearchForm.css';
+import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
+import search from "../../images/search-form__button.svg";
+import lens from "../../images/lens-icon.svg";
+
+function SearchForm (props) {
+  const [searchFilter, setSearchFilter] = useState(props.searchFilter);
+
+  return (
+    <section className="search-form">
+      <div className="search-form__container">
+        <label className="search-form__input-group">
+          <img className="search-form__lens-image" alt='lens icon' src={ lens }/>
+          <input
+            className="search-form__input"
+            name="search"
+            type="text"
+            placeholder="Фильм"
+            autoComplete="off"
+            onChange={ e => setSearchFilter(e.target.value) }
+            required
+          />
+          <button className="search-form__button" onClick={ () => props.setSearchFilter(searchFilter) }>
+            <img className="search-form__button-image" alt='search movie' src={ search }/>
+          </button>
+        </label>
+        <FilterCheckbox
+          className="search-form__filter-checkbox-wide"
+          shortFilms={ props.shortFilms }
+          setShortFilms={ props.setShortFilms }/>
+      </div>
+      <FilterCheckbox
+          className="search-form__filter-checkbox-narrow"
+          shortFilms={ props.shortFilms }
+          setShortFilms={ props.setShortFilms }/>
+      <div className="search-form__divider"/>
+    </section>
+  )
+}
+
+export default SearchForm; 
